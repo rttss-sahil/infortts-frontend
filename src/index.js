1ca6dd04392ca96dc844dac3c341e5a8e9405326
Sahil Rathee (rttss) <sahil.git@outlook.com>
2021-05-15 17:21:14 +0530

    cvcv

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import reportWebVitals from './reportWebVitals';
 import store from './app/store';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import * as serviceWorker from './serviceWorker';
 // eslint-disable-next-line
 import registerServiceWorker from 'react-service-worker';
 ReactDOM.render(
@@ -14,7 +15,7 @@ ReactDOM.render(
   ,
   document.getElementById('root')
 );
-
+serviceWorker.register()
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
